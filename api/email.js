5763829c265d196b4a221608c0faeea3d604f2f4
israel92305 <israellubanda923@gmail.com>
2026-05-28 23:50:34 +0200

    changed proxy email.js

--- api/email.js
+++ api/email.js
@@ -1,1 +1,9 @@
-export { default } from "../BackEnd/api/email.js";
+import { createRequire } from "module";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+const handler = await import(resolve(__dirname, "../BackEnd/api/email.js"));
+
+export default handler.default;
